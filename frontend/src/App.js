4f1d7fdf797af009b1e52288a5097ff55a485b1c
daniloherreraf <herreraf.danilo@gmail.com>
2023-03-16 17:49:46 -0500

    Create-Data-Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
-       <a href="/">amazona</a>
+        <a href="/">amazona</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className='products'>
+        {data.products.map((product) => (
+          <div className='product' key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </a>
+            <div className='product-info'>
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+            <p><strong>$ {product.price}</strong></p>
+            <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+        </div>
       </main>
     </div>
   );
